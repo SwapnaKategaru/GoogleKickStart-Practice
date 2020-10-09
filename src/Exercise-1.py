a738fa70bffbc333355e0534980a158137bc3d8b
ASUS <swapnake123@gmail.com>
2020-10-09 23:50:04 +0530

    Commited two practice problems.

--- src/Exercise-1.py
+++ src/Exercise-1.py
@@ -27,7 +27,18 @@ for j in range(1, T + 1):
 Case #1: 4
 3 1000
 1111 1111 1112
-Case #2: 0 """
+Case #2: 0 
+
+3
+4 100
+20 90 40 90
+Case #1: 2
+4 50
+30 30 10 10
+Case #2: 3
+3 300
+999 999 999
+Case #3: 0 """
 
 
 # ################### Problem 2 ###################
@@ -37,11 +48,11 @@ for x in range(1, T + 1):
     N, P = map(int, input().split())
     S = map(int, input().split())
     S = sorted(S, reverse=True)
-    y = hours = sum(S[0] - s for s in S[:P])
+    hours = sum(S[0] - s for s in S[:P])
     for i in range(1, N - P + 1):
         hours -= (S[i - 1] - S[i]) * (P - 1)
         hours += S[i] - S[P + i - 1]
-        if hours < y:
+        if hours < y:3
             y = hours
     print("Case #{}: {}".format(x, y), flush=True)
 
@@ -56,3 +67,8 @@ Case #2: 0
 5 5
 7 7 1 7 7
 Case #3: 6 """
+
+
+# ################### Problem 3 ###################
+
+
